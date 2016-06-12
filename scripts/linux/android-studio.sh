3248d34ceb164c0c86ea2e15b6710b7aa98ffd0f
Ryan Davis <ryan.davis.dev@gmail.com>
2016-06-12 11:55:51 +1000

    Add android studio launcher and environment setup

---
+++ scripts/linux/android-studio.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true'
+export JAVA_HOME='/opt/java/jdk'
+/opt/android-studio/bin/studio.sh
