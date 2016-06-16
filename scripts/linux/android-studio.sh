2a4e7be663fd1cb4fd5f8a1d64e1522c6ba7087f
Ryan Davis <ryan.davis.dev@gmail.com>
2016-06-16 19:01:47 +1000

    Move default android studio config path

--- scripts/linux/android-studio.sh
+++ scripts/linux/android-studio.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true'
+export _JAVA_OPTIONS="
+    -Dawt.useSystemAAFontSettings=on
+    -Dswing.aatext=true
+    -Dsun.java2d.xrender=true
+    -Didea.config.path=${XDG_CONFIG_HOME}/android-studio/config
+    -Didea.system.path=${XDG_CONFIG_HOME}/android-studio/system
+"
 export JAVA_HOME='/opt/java/jdk'
 /opt/android-studio/bin/studio.sh
