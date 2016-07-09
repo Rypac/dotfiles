2930ac1a2585d197ff218b414edcf13d73ea18d5
Ryan Davis <ryan.davis.dev@gmail.com>
2016-07-09 14:06:51 +1000

    Terminate with successful error code for i3blocks compatability

--- scripts/rofi/i3-logout.sh
+++ scripts/rofi/i3-logout.sh
@@ -5,6 +5,8 @@ options="Lock|Sleep|Logout|Reboot|Shutdown|Cancel"
 selection=$(echo $options | rofi -dmenu -sep '|' -p 'Select option: ')
 exit_option=$(echo "$selection" | tr '[:upper:]' '[:lower:]')
 
-if [ ! $selection = "Cancel" ]; then
+if [ ! "$selection" = "Cancel" ]; then
     "$HOME/scripts/i3/exit.sh" $exit_option
 fi
+
+exit 0
