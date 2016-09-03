5f9bd5208d1b8f4572eab573fb7699b0b06f15f0
Ryan Davis <ryan.davis.dev@gmail.com>
2016-09-03 17:26:30 +1000

    Add floating terminal scratchpads

---
+++ scripts/i3/toggle-scratchpad.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+function usage() {
+    echo "Usage: $(basename "$0") NAME [COMMAND]"
+}
+
+function scratchpad_exists() {
+    i3-msg -t get_tree | grep -Po "\"instance\":\"$1\""
+}
+
+function create_scratchpad() {
+    [[ ! -z "$2" ]] && args="-e $2" || args=""
+    i3-msg -q "exec --no-startup-id urxvt -name $1 $args" && sleep 0.2
+    i3-msg -q [instance="$1"] border none
+    i3-msg -q [instance="$1"] floating enable
+    i3-msg -q [instance="$1"] move scratchpad
+}
+
+function show_scratchpad() {
+    i3-msg -q [instance="$1"] scratchpad show
+}
+
+if [[ $# -lt 1 ]]; then
+    usage && exit 1
+fi
+
+name="$1"
+program="$2"
+
+if ! scratchpad_exists "$name" > /dev/null 2>&1; then
+    create_scratchpad "$name" "$program"
+fi
+
+show_scratchpad "$name"
