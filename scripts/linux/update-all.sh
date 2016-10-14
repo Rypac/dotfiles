cacd8208817321a88bbc360656e00132137dce0e
Ryan Davis <void@rdavis.xyz>
2016-10-14 20:20:40 +1100

    Add script to easily update apps from all package managers

---
+++ scripts/linux/update-all.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# dotfiles
+update-dotfiles
+
+# apt packages
+sudo apt update
+sudo apt upgrade
+
+# non-apt installed packages
+local-packages update
+local-packages install
+
+# rust
+rustup update
+
+# stack
+stack --resolver nightly update
+stack --resolver nightly upgrade
+
+# npm
+npm update -g
+
+# python
+pip3 list --outdated --user | cut -d ' ' -f 1 | xargs -n 1 pip3 install --upgrade --user
+
+# vim
+nvim +PlugUpgrade +PlugInstall +PlugUpdate +qall
