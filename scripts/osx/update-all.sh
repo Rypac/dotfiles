b9ae722a79293d5954eca4d38afcbbc47c118f1f
Ryan Davis <void@rdavis.xyz>
2016-10-29 10:52:57 +1100

    Add update-all script for osx

---
+++ scripts/osx/update-all.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# dotfiles
+update-dotfiles
+
+# apt packages
+brew update
+brew upgrade
+
+# non-tracked packages
+"${XDG_CONFIG_HOME}/fzf/install" --bin
+
+# rust
+rustup update
+
+# stack
+stack --resolver nightly update
+stack --resolver nightly upgrade
+
+# npm
+n-update -y
+n stable
+npm update -g
+
+# python
+pip3 list --outdated | cut -d ' ' -f 1 | xargs -n 1 pip3 install --upgrade
+
+# vim
+nvim +PlugUpgrade +PlugInstall +PlugUpdate +qall
