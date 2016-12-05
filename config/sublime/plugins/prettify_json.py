7c55c694d10775c34c78b0c7d9799fe4863364bc
Ryan Davis <void@rdavis.xyz>
2016-12-05 20:52:05 +1100

    Ensure user plugins are seen by sublime

--- config/sublime/plugins/prettify_json.py
+++ config/sublime/plugins/prettify_json.py
@@ -5,8 +5,9 @@ from collections import OrderedDict
 
 class PrettifyJsonCommand(sublime_plugin.TextCommand):
     def run(self, edit, indent=4):
-        for region in filter(lambda sel: not sel.empty(), self.view.sel()):
-            text = self.view.substr(region)
-            ugly_json = json.loads(text, object_pairs_hook=OrderedDict)
-            pretty_json = json.dumps(ugly_json, indent=indent)
-            self.view.replace(edit, region, pretty_json)
+        for region in self.view.sel():
+            if not region.empty():
+                text = self.view.substr(region)
+                ugly_json = json.loads(text, object_pairs_hook=OrderedDict)
+                pretty_json = json.dumps(ugly_json, indent=indent)
+                self.view.replace(edit, region, pretty_json)
