93d34c22a6ce2a142ceef4d6388e7c26b49004c4
Ryan Davis <me@rdavis.xyz>
2017-02-15 20:50:16 +1300

    Clean up prepend and append to path functions

--- fish/functions/append_to_path.fish
+++ fish/functions/append_to_path.fish
@@ -1,12 +1,7 @@
 function append_to_path --description 'Append the value(s) to $PATH'
-    if test (count $argv) = 1
-        set -l value $argv[1]
-        if test -d $value; and not contains -- $value $PATH
+    for value in $argv
+        if test -d $value; and not contains $value $PATH
             set PATH $PATH $value
-        end
-    else
-        for value in $argv
-            prepend_to_path $value
         end
     end
 end
--- fish/functions/prepend_to_path.fish
+++ fish/functions/prepend_to_path.fish
@@ -1,12 +1,7 @@
 function prepend_to_path --description 'Prepend the value(s) to $PATH'
-    if test (count $argv) = 1
-        set -l value $argv[1]
-        if test -d $value; and not contains -- $value $PATH
+    for value in $argv
+        if test -d $value; and not contains $value $PATH
             set PATH $value $PATH
-        end
-    else
-        for value in $argv
-            prepend_to_path $value
         end
     end
 end
