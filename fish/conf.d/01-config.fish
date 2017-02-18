503ba4e7b3b3ec07857daccc8486eb62b3b5e96a
Ryan Davis <me@rdavis.xyz>
2017-02-19 09:49:17 +1300

    Use setenv to export environment variables

--- fish/conf.d/01-config.fish
+++ fish/conf.d/01-config.fish
@@ -3,11 +3,11 @@ if not status --is-interactive
 end
 
 # Ensure directories exist
-if test ! -d "$XDG_CONFIG_HOME"
+if not test -d "$XDG_CONFIG_HOME"
     mkdir -p "$XDG_CONFIG_HOME"
 end
 
-if test ! -d "$XDG_DATA_HOME"
+if not test -d "$XDG_DATA_HOME"
     mkdir -p "$XDG_DATA_HOME"
 end
 
@@ -15,17 +15,17 @@ end
 fish_vi_key_bindings
 
 # fzf
-if test ! -d "$XDG_DATA_HOME/fzf"
+if not test -d "$XDG_DATA_HOME/fzf"
     git clone --depth 1 https://github.com/junegunn/fzf.git "$XDG_DATA_HOME/fzf"
     eval "$XDG_DATA_HOME/fzf/install --bin"
 end
 
 prepend_to_path "$XDG_DATA_HOME/fzf/bin"
-set -gx FZF_LEGACY_KEYBINDINGS 0
-set -gx FZF_TMUX 1
+setenv FZF_LEGACY_KEYBINDINGS 0
+setenv FZF_TMUX 1
 
 # base16 colour scheme
-if test ! -d "$XDG_DATA_HOME/base16-shell"
+if not test -d "$XDG_DATA_HOME/base16-shell"
     git clone --depth 1 https://github.com/chriskempson/base16-shell.git "$XDG_DATA_HOME/base16-shell"
 end
 
@@ -35,26 +35,21 @@ set -l os (uname)
 
 if [ "$os" = "Linux" ]
     # Rust
-    set -gx RUST_SRC_PATH "$RUSTUP_HOME/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"
+    setenv RUST_SRC_PATH "$RUSTUP_HOME/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"
 
     # Android SDK/NDK
-    set -gx JAVA_HOME '/opt/java/jdk'
+    setenv JAVA_HOME '/opt/java/jdk'
 
 else if [ "$os" = "Darwin" ]
     # Rust
-    set -gx RUST_SRC_PATH "$RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src"
+    setenv RUST_SRC_PATH "$RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src"
 
     # Android SDK/NDK
-    set -gx JAVA_HOME '/Library/Java/JavaVirtualMachines/jdk1.8.0_92.jdk/Contents/Home'
-
-    # Ruby
-    if type -q rbenv
-        source (rbenv init -|psub)
-    end
+    setenv JAVA_HOME '/Library/Java/JavaVirtualMachines/jdk1.8.0_92.jdk/Contents/Home'
 end
 
 if test -z "$GPG_TTY"
-    set -gx GPG_TTY (tty)
+    setenv GPG_TTY (tty)
 end
 
 append_to_path "$JAVA_HOME/bin"
