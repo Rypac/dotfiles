a7298b45ee0262ba0bf32e654f84768557378871
Ryan Davis <me@rdavis.xyz>
2017-02-19 18:06:56 +1300

    More alias trimming

--- fish/conf.d/02-aliases.fish
+++ fish/conf.d/02-aliases.fish
@@ -10,11 +10,9 @@ alias vim nvim
 alias mux tmuxinator
 
 alias gpg gpg2
-alias to-me 'gpg --encrypt --recipient me@rdavis.xyz --armour'
-alias scramble 'gpg --output encrypted.gpg --encrypt --recipient me@rdavis.xyz'
+alias scramble 'gpg --encrypt --recipient me@rdavis.xyz --armour'
 alias unscramble 'gpg --decrypt --quiet'
 
-alias stack-update 'stack --resolver nightly setup --reinstall --install-ghc'
 alias nstack 'stack --resolver nightly'
 
 alias vscode code
@@ -22,7 +20,6 @@ alias vscode code
 switch (uname)
     case Linux
         alias pip-upgrade "pip3 list --outdated --user --format=legacy | cut -d ' ' -f 1 | xargs -n 1 pip3 install --upgrade --user"
-
         alias code "code --user-data-dir "$XDG_CONFIG_HOME/vscode" --extensions-dir "$XDG_CONFIG_HOME/vscode/extensions""
     case Darwin
         alias pip-upgrade "pip3 list --outdated --format=legacy | cut -d ' ' -f 1 | xargs -n 1 pip3 install --upgrade"
