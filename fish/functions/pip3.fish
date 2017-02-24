9ba095bc282ec29e2fec81ddbe8a81440121006f
Ryan Davis <me@rdavis.xyz>
2017-02-25 09:35:59 +1100

    Fix installation of user pip packages

--- fish/functions/pip3.fish
+++ fish/functions/pip3.fish
@@ -6,7 +6,7 @@ function pip3
         case install list
             command pip3 $cmd --user $argv
         case update upgrade
-            pip3 list --outdated --format=legacy $argv | cut -d ' ' -f 1 | xargs -n 1 pip3 install --upgrade $argv
+            command pip3 list --user --outdated --format=legacy $argv | cut -d ' ' -f 1 | xargs -n 1 pip3 install --user --upgrade $argv
         case '*'
             command pip3 $cmd $argv
     end
