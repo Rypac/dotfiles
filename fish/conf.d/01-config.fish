a4e0200d930f59598f36c31e7f9eb15f8b30049f
Ryan Davis <me@rdavis.xyz>
2017-02-25 11:18:17 +1100

    Add default fish colours

--- fish/conf.d/01-config.fish
+++ fish/conf.d/01-config.fish
@@ -13,6 +13,16 @@ end
 # vim bindings
 fish_vi_key_bindings
 
+# colours
+set -gx fish_color_autosuggestion "brblack"
+set -gx fish_color_command "purple"
+set -gx fish_color_comment "yellow"
+set -gx fish_color_end "purple"
+set -gx fish_color_error "red"
+set -gx fish_color_param "blue"
+set -gx fish_color_quote "green"
+set -gx fish_color_redirection "cyan"
+
 # fzf
 if not test -d "$XDG_DATA_HOME/fzf"
     git clone --depth 1 https://github.com/junegunn/fzf.git "$XDG_DATA_HOME/fzf"
