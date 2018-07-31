f6bfe4d28af1d12eba9c2e4e48b04135f611a726
Ryan Davis <ryan@rypac.org>
2018-07-31 18:28:02 +1000

    Show git status in fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -6,9 +6,20 @@ function fish_prompt
     set -l green (set_color -o green)
     set -l normal (set_color normal)
 
-    set -l prompt_symbol '$'
-    if [ $USER = 'root' ]
-        set prompt_symbol '#'
+    set -g __fish_git_prompt_color_branch brmagenta
+    set -g __fish_git_prompt_color_cleanstate brgreen
+    set -g __fish_git_prompt_color_invalidstate red
+    set -g __fish_git_prompt_color_stagedstate yellow
+    set -g __fish_git_prompt_show_informative_status 1 end
+    set -g __fish_git_prompt_showupstream "informative"
+    set -g __fish_git_prompt_showdirtystate "yes"
+
+    set -l prompt_symbol
+    switch "$USER"
+        case root toor
+            set prompt_symbol '#'
+        case '*'
+            set prompt_symbol '$'
     end
 
     set -l prompt_colour $green
@@ -16,8 +27,9 @@ function fish_prompt
         set prompt_colour $red
     end
 
-    set -l cwd $blue(basename (prompt_pwd))
-    set -l prompt $prompt_colour$prompt_symbol
+    set -l cwd $blue(basename (prompt_pwd))$normal
+    set -l git (__fish_git_prompt)
+    set -l prompt $prompt_colour$prompt_symbol$normal
 
-    echo -n $cwd $prompt $normal
+    echo -n -s $cwd $git " $prompt "
 end
