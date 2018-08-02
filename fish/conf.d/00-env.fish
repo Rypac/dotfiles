56ea105e595c0769547d7824b631755f50d55738
Ryan Davis <ryan@rypac.org>
2018-08-03 08:42:29 +1000

    Disable dotnet telemetry

--- fish/conf.d/00-env.fish
+++ fish/conf.d/00-env.fish
@@ -64,6 +64,7 @@ set -gx LESS '-F -g -i -M -R -S -w -X -z-4'
 set -gx SHELL '/usr/local/bin/fish'
 set -gx HUSHLOGIN_FILE ''
 set -gx HOMEBREW_NO_ANALYTICS 1
+set -gx DOTNET_CLI_TELEMETRY_OPTOUT 1
 set -gx AUTOJUMP_SOURCED 1
 set -gx FZF_TMUX 1
 
