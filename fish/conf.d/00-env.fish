8858be70d233ccc30ca6dc9323fcfe6e01cc7a83
Ryan Davis <ryan@rypac.org>
2020-04-28 11:26:11 +1000

    Support older versions of fish

--- fish/conf.d/00-env.fish
+++ fish/conf.d/00-env.fish
@@ -1,8 +1,8 @@
 # Setup XDG environment variables
-set -q "$XDG_DATA_HOME" || set -gx XDG_DATA_HOME "$HOME/.local/share"
-set -q "$XDG_CONFIG_HOME" || set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -q "$XDG_CACHE_HOME" || set -gx XDG_CACHE_HOME "$HOME/.cache"
-set -q "$DOTFILES_HOME" || set -gx DOTFILES_HOME "$HOME/.dotfiles"
+set -q "$XDG_DATA_HOME"; or set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -q "$XDG_CONFIG_HOME"; or set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -q "$XDG_CACHE_HOME"; or set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -q "$DOTFILES_HOME"; or set -gx DOTFILES_HOME "$HOME/.dotfiles"
 
 # Set config/data/cache directories for XDG unfriendly programs
 set -gx AWS_CONFIG_FILE "$XDG_CONFIG_HOME/aws/config"
