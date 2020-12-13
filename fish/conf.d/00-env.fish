97e047313de1b2608cc04da4fd682c0cda99e746
Ryan Davis <ryan@rypac.org>
2020-12-14 08:30:24 +1100

    Add XDG_BIN_HOME environment variable

--- fish/conf.d/00-env.fish
+++ fish/conf.d/00-env.fish
@@ -1,4 +1,5 @@
 # Setup XDG environment variables
+set -q "$XDG_BIN_HOME"; or set -gx XDG_BIN_HOME "$HOME/.local/bin"
 set -q "$XDG_DATA_HOME"; or set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -q "$XDG_CONFIG_HOME"; or set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -q "$XDG_CACHE_HOME"; or set -gx XDG_CACHE_HOME "$HOME/.cache"
