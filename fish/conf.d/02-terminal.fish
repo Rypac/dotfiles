646a74404c9b17a5fdc36225ed53b08475197841
Ryan Davis <ryan@rypac.org>
2021-09-17 22:46:28 +1000

    Add XDG_STATE_HOME for history and log files

--- fish/conf.d/02-terminal.fish
+++ fish/conf.d/02-terminal.fish
@@ -2,6 +2,6 @@ set -gx LC_COLLATE 'C'
 set -gx EDITOR 'vim'
 set -gx VISUAL 'subl -w'
 set -gx PAGER 'less'
-set -gx LESSHISTFILE "$XDG_CACHE_HOME/lesshistory"
+set -gx LESSHISTFILE "$XDG_STATE_HOME/lesshst"
 set -gx SELECTED_EDITOR $EDITOR
 set -gx HOMEBREW_NO_ANALYTICS 1
--- fish/conf.d/20-node.fish
+++ fish/conf.d/20-node.fish
@@ -1,4 +1,4 @@
 if type -q node
-    set -gx NODE_REPL_HISTORY "$XDG_CACHE_HOME/node_repl_history"
+    set -gx NODE_REPL_HISTORY "$XDG_STATE_HOME/node_repl_history"
     set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/config"
 end
