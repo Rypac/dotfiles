faf7fcf7124b5567a980703764c62314aa1159d4
Ryan Davis <ryan@rypac.org>
2021-09-18 10:22:05 +1000

    Clean up environment variables

--- fish/conf.d/02-terminal.fish
+++ fish/conf.d/02-terminal.fish
@@ -1,7 +1,6 @@
-set -gx LC_COLLATE 'C'
-set -gx EDITOR 'vim'
+set -gx LC_COLLATE C
+set -gx EDITOR vim
 set -gx VISUAL 'subl -w'
-set -gx PAGER 'less'
+set -gx PAGER less
 set -gx LESSHISTFILE "$XDG_STATE_HOME/lesshst"
-set -gx SELECTED_EDITOR $EDITOR
 set -gx HOMEBREW_NO_ANALYTICS 1
