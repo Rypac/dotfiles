fd1b92ad740e9c6ea37c7af4f9bb34c0ccb33920
Ryan Davis <ryan@rypac.org>
2021-11-20 10:11:52 +1100

    Add cabal environment variables

--- fish/conf.d/20-haskell.fish
+++ fish/conf.d/20-haskell.fish
@@ -1,5 +1,10 @@
 set -gx GHCUP_USE_XDG_DIRS 1
 
+if command -q cabal
+    set -gx CABAL_DIR "$XDG_DATA_HOME/cabal"
+    fish_add_path --append "$CABAL_DIR/bin"
+end
+
 if command -q stack
     set -gx STACK_ROOT "$XDG_DATA_HOME/stack"
 end
