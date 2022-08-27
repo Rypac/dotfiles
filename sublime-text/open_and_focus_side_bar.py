f3c3414d3b2e26affe508c09ea9c407268f0a0bf
Ryan Davis <ryan@rypac.org>
2022-08-27 16:18:40 +1000

    Remove unused Sublime Text custom plugins

--- sublime-text/open_and_focus_side_bar.py
+++
@@ -1,8 +0,0 @@
-import sublime
-import sublime_plugin
-
-
-class OpenAndFocusSideBarCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.set_sidebar_visible(True)
-        sublime.set_timeout_async(lambda: self.window.run_command('focus_side_bar'), 100)
