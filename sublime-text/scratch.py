f7a9b15233481ff89f8ffd6fc4fcac421942de2b
Ryan Davis <ryan@rypac.org>
2023-04-03 07:14:30 +1000

    Replace enable/disable with toggle command

--- sublime-text/scratch.py
+++ sublime-text/scratch.py
@@ -2,17 +2,6 @@ import sublime
 import sublime_plugin
 
 
-class EnableViewScratchCommand(sublime_plugin.TextCommand):
+class ToggleViewScratchCommand(sublime_plugin.TextCommand):
     def run(self, edit: sublime.Edit) -> None:
-        self.view.set_scratch(True)
-
-    def is_visible(self) -> bool:
-        return not self.view.is_scratch()
-
-
-class DisableViewScratchCommand(sublime_plugin.TextCommand):
-    def run(self, edit: sublime.Edit) -> None:
-        self.view.set_scratch(False)
-
-    def is_visible(self) -> bool:
-        return self.view.is_scratch()
+        self.view.set_scratch(not self.view.is_scratch())
