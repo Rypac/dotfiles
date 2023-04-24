2ccc38080cdc7aaeee7ed65689a0c649973f15b5
Ryan Davis <ryan@rypac.org>
2023-04-24 18:51:58 +1000

    Add neovim configuration

---
+++ nvim/init.lua
@@ -0,0 +1,22 @@
+-- UI
+vim.opt.number = true
+
+-- Searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Tabs
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Whitespace
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
+-- Ignore
+vim.opt.wildignore = { ".git/", "node_modules/" }
+
+-- Keybindings
+vim.g.mapleader = " "
