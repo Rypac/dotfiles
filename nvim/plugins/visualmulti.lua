9acc9dd66bb92469f8d6d373320cadcd5babbed3
Ryan Davis <ryan@rypac.org>
2023-04-26 12:58:03 +1000

    Add more visual-multi key bindings

--- nvim/plugins/visualmulti.lua
+++ nvim/plugins/visualmulti.lua
@@ -5,5 +5,7 @@ return {
   keys = {
     { "<M-j>", ":call vm#commands#add_cursor_down(0, v:count1)<cr>" },
     { "<M-k>", ":call vm#commands#add_cursor_up(0, v:count1)<cr>" },
+    { "<M-Down>", ":call vm#commands#add_cursor_down(0, v:count1)<cr>" },
+    { "<M-Up>", ":call vm#commands#add_cursor_up(0, v:count1)<cr>" },
   },
 }
