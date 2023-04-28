43f8f9407d6a9d9abc0eae3ce65286750bb0ac0c
Ryan Davis <ryan@rypac.org>
2023-04-28 10:10:16 +1000

    Replace neotree with oil

---
+++ nvim/plugins/oil.lua
@@ -0,0 +1,34 @@
+return {
+  "stevearc/oil.nvim",
+  lazy = false,
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      win_options = {
+        winblend = 0,
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("oil").open()
+      end,
+      desc = "Open Directory",
+    },
+    {
+      "<leader>E",
+      function()
+        require("oil").open_float()
+      end,
+      desc = "Open Directory (Float)",
+    },
+  },
+  init = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
