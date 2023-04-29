435ecf318d4d843b382fbf533592f8b0fe099fd3
Ryan Davis <ryan@rypac.org>
2023-04-29 12:04:14 +1000

    Use Base16 Oceanic Next theme

--- nvim/plugins/colorscheme.lua
+++ nvim/plugins/colorscheme.lua
@@ -1,8 +1,47 @@
 return {
-  "arcticicestudio/nord-vim",
+  "echasnovski/mini.base16",
+  version = "*",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme nord]])
+  opts = {
+    palette = {
+      base00 = "#1B2B34",
+      base01 = "#343D46",
+      base02 = "#4F5B66",
+      base03 = "#65737E",
+      base04 = "#A7ADBA",
+      base05 = "#C0C5CE",
+      base06 = "#CDD3DE",
+      base07 = "#D8DEE9",
+      base08 = "#EC5f67",
+      base09 = "#F99157",
+      base0A = "#FAC863",
+      base0B = "#99C794",
+      base0C = "#5FB3B3",
+      base0D = "#6699CC",
+      base0E = "#C594C5",
+      base0F = "#AB7967",
+    },
+    use_cterm = {
+      base00 = 00,
+      base01 = 18,
+      base02 = 19,
+      base03 = 08,
+      base04 = 20,
+      base05 = 07,
+      base06 = 21,
+      base07 = 15,
+      base08 = 01,
+      base09 = 16,
+      base0A = 03,
+      base0B = 02,
+      base0C = 06,
+      base0D = 04,
+      base0E = 05,
+      base0F = 17,
+    },
+  },
+  config = function(_, opts)
+    require("mini.base16").setup(opts)
   end,
 }
