0919eec7f757848068cd0139612027dd81849fc6
Ryan Davis <ryan@rypac.org>
2023-05-01 16:16:34 +1000

    Update theme and keybindings vim-visual-multi

--- nvim/plugins/visualmulti.lua
+++ nvim/plugins/visualmulti.lua
@@ -7,5 +7,11 @@ return {
     { "<M-k>", ":call vm#commands#add_cursor_up(0, v:count1)<cr>" },
     { "<M-Down>", ":call vm#commands#add_cursor_down(0, v:count1)<cr>" },
     { "<M-Up>", ":call vm#commands#add_cursor_up(0, v:count1)<cr>" },
+    { "<C-LeftMouse>", "<Plug>(VM-Mouse-Cursor)" },
+    { "<C-RightMouse>", "<Plug>(VM-Mouse-Word)" },
+    { "<M-C-RightMouse>", "<Plug>(VM-Mouse-Column)" },
   },
+  config = function()
+    vim.g.VM_theme = "nord"
+  end,
 }
