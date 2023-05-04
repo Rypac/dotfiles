e17a3a0b322e908964ca03922e52d1ddc9732ca3
Ryan Davis <ryan@rypac.org>
2023-05-04 21:55:40 +1000

    Add gitsigns plugin

--- nvim/plugins/ui.lua
+++ nvim/plugins/ui.lua
@@ -39,6 +39,8 @@ return {
         ["<leader>ghr"] = { name = "+review" },
         ["<leader>ghp"] = { name = "+pull request" },
         ["<leader>ght"] = { name = "+thread" },
+        ["<leader>h"] = { name = "+hunk" },
+        ["<leader>ht"] = { name = "+toggle" },
         ["<leader>f"] = { name = "+file/find" },
         ["<leader>s"] = { name = "+search" },
         ["<leader>t"] = { name = "+tabs" },
@@ -55,6 +57,29 @@ return {
       wk.setup(opts)
       wk.register(opts.defaults)
     end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    cmd = "Gitsigns",
+    keys = {
+      { "<leader>hs", "<cmd>Gitsigns stage_hunk<cr>", desc = "Stage Hunk", mode = { "n", "v" } },
+      { "<leader>hr", "<cmd>Gitsigns reset_hunk<cr>", desc = "Reset Hunk", mode = { "n", "v" } },
+      { "<leader>hp", "<cmd>Gitsigns preview_hunk<cr>", desc = "Preview Hunk" },
+      { "<leader>hb", "<cmd>lua require('gitsigns').blame_line({full=true})<cr>", desc = "Blame" },
+      { "<leader>hd", "<cmd>Gitsigns diffthis<cr>", desc = "Diff" },
+      { "<leader>hD", "<cmd>lua require('gitsigns').diffthis('~')<cr>", desc = "Diff All" },
+      { "<leader>htb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Blame" },
+      { "<leader>htd", "<cmd>Gitsigns toggle_deleted<cr>", desc = "Deleted" },
+      { "<leader>htl", "<cmd>Gitsigns toggle_linehl<cr>", desc = "Highlight Line" },
+      { "<leader>htn", "<cmd>Gitsigns toggle_numhl<cr>", desc = "Highlight Number" },
+      { "<leader>hts", "<cmd>Gitsigns toggle_signs<cr>", desc = "Signs" },
+      { "<leader>htw", "<cmd>Gitsigns toggle_word_diff<cr>", desc = "Word Diff" },
+    },
+    config = true,
   },
   {
     "folke/zen-mode.nvim",
