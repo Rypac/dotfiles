e811a99d6c9c2f53cac1ecf84360756078050a14
Ryan Davis <ryan@rypac.org>
2023-05-05 08:17:09 +1000

    Update lsp formatters

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -79,10 +79,10 @@ return {
       local null_ls = require("null-ls")
       return {
         sources = {
+          null_ls.builtins.formatting.black,
           null_ls.builtins.formatting.fourmolu,
           null_ls.builtins.formatting.jq,
           null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.ruff,
           null_ls.builtins.formatting.stylua.with({
             extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
           }),
