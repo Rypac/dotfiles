e3dcd020059953c2f7f3dfee9632a75df3292bcf
Ryan Davis <ryan@rypac.org>
2023-05-05 21:31:17 +1000

    Set lsp keybindings in on_attach callback

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -1,3 +1,50 @@
+local on_attach = function(client, bufnr)
+  keys = {
+    { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
+    { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition" },
+    { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
+    { "gy", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Goto Type Definition" },
+    { "gi", "<cmd>Telescope lsp_implementations<cr>", desc = "Goto Implementation" },
+    { "K", vim.lsp.buf.hover, desc = "Hover" },
+    { "gK", vim.lsp.buf.signature_help, desc = "Signature Help" },
+    { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help" },
+    { "<leader>cd", vim.diagnostic.open_float, desc = "Line Diagnostics" },
+    { "<leader>cr", vim.lsp.buf.rename, desc = "Rename Symbol" },
+    { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
+    { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" } },
+    {
+      "<leader>cf",
+      function()
+        vim.lsp.buf.format({ async = true })
+      end,
+      desc = "Format Document",
+    },
+    {
+      "<leader>cf",
+      function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+      desc = "Format Range",
+      mode = "v",
+    },
+    {
+      "<leader>ca",
+      vim.lsp.buf.code_action,
+      desc = "Code Action",
+      mode = { "n", "v" },
+    },
+  }
+
+  for _, key in pairs(keys) do
+    vim.keymap.set(key.mode or "n", key[1], key[2], {
+      desc = key.desc,
+      buffer = bufnr,
+      silent = true,
+      noremap = true,
+    })
+  end
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -25,46 +72,13 @@ return {
         sourcekit = {},
       },
     },
-    keys = {
-      { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
-      { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition" },
-      { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
-      { "gT", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Goto Type Definition" },
-      { "gI", "<cmd>Telescope lsp_implementations<cr>", desc = "Goto Implementation" },
-      { "K", vim.lsp.buf.hover, desc = "Hover" },
-      { "gK", vim.lsp.buf.signature_help, desc = "Signature Help" },
-      { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help" },
-      { "<leader>cd", vim.diagnostic.open_float, desc = "Line Diagnostics" },
-      { "<leader>cr", vim.lsp.buf.rename, desc = "Rename Symbol" },
-      { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
-      {
-        "<leader>cf",
-        function()
-          vim.lsp.buf.format({ async = true })
-        end,
-        desc = "Format Document",
-      },
-      {
-        "<leader>cf",
-        function()
-          vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-        end,
-        desc = "Format Range",
-        mode = "v",
-      },
-      {
-        "<leader>ca",
-        vim.lsp.buf.code_action,
-        desc = "Code Action",
-        mode = { "n", "v" },
-      },
-    },
     config = function(_, opts)
       local lspconfig = require("lspconfig")
 
       local servers = opts.servers or {}
       for server, server_opts in pairs(servers) do
-        lspconfig[server].setup(server_opts)
+        local merged_opts = vim.tbl_deep_extend("force", {}, server_opts, { on_attach = on_attach })
+        lspconfig[server].setup(merged_opts)
       end
     end,
   },
@@ -78,8 +92,10 @@ return {
     opts = function()
       local null_ls = require("null-ls")
       return {
+        on_attach = on_attach,
         sources = {
           null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.cabal_fmt,
           null_ls.builtins.formatting.fourmolu,
           null_ls.builtins.formatting.jq,
           null_ls.builtins.formatting.prettier,
