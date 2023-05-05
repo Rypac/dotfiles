56c274e4251306a7e45987ed12813516e2a6cd8d
Ryan Davis <ryan@rypac.org>
2023-05-05 14:42:45 +1000

    Add treesitter plugins

--- nvim/plugins/treesitter.lua
+++ nvim/plugins/treesitter.lua
@@ -1,59 +1,130 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  version = false,
-  build = ":TSUpdate",
-  event = {
-    "BufReadPost",
-    "BufNewFile",
-  },
-  opts = {
-    highlight = {
-      enable = true,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false,
+    build = ":TSUpdate",
+    event = {
+      "BufReadPost",
+      "BufNewFile",
     },
-    indent = {
-      enable = true,
-    },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-    ensure_installed = {
-      "bash",
-      "c",
-      "haskell",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "luadoc",
-      "luap",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "regex",
-      "rust",
-      "sql",
-      "swift",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = "<nop>",
-        node_decremental = "<bs>",
+    opts = {
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+      },
+      ensure_installed = {
+        "bash",
+        "c",
+        "haskell",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "sql",
+        "swift",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = "<nop>",
+          node_decremental = "<bs>",
+        },
       },
     },
+    keys = {
+      { "<c-space>", desc = "Increment selection" },
+      { "<bs>", desc = "Decrement selection", mode = "x" },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
   },
-  keys = {
-    { "<c-space>", desc = "Increment selection" },
-    { "<bs>", desc = "Decrement selection", mode = "x" },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    version = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = {
+      "BufReadPost",
+      "BufNewFile",
+    },
+    config = function()
+      require("treesitter-context").setup()
+    end,
   },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    version = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = {
+      "BufReadPost",
+      "BufNewFile",
+    },
+    opts = {
+      textobjects = {
+        select = {
+          enable = true,
+          keymaps = {
+            ["af"] = { query = "@function.outer", desc = "a function" },
+            ["if"] = { query = "@function.inner", desc = "inner function" },
+            ["ac"] = { query = "@class.outer", desc = "a class" },
+            ["ic"] = { query = "@class.inner", desc = "inner class" },
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>csn"] = { query = "@parameter.inner", desc = "Swap next parameter" },
+          },
+          swap_previous = {
+            ["<leader>csp"] = { query = "@parameter.inner", desc = "Swap previous parameter" },
+          },
+        },
+        move = {
+          enable = true,
+          goto_next_start = {
+            ["]m"] = { query = "@function.outer", desc = "Next function start" },
+            ["]]"] = { query = "@class.outer", desc = "Next class start" },
+          },
+          goto_next_end = {
+            ["]M"] = { query = "@function.outer", desc = "Next function end" },
+            ["]["] = { query = "@class.outer", desc = "Next class end" },
+          },
+          goto_previous_start = {
+            ["[m"] = { query = "@function.outer", desc = "Previous function start" },
+            ["[["] = { query = "@class.outer", desc = "Previous class start" },
+          },
+          goto_previous_end = {
+            ["[M"] = { query = "@function.outer", desc = "Previous function end" },
+            ["[]"] = { query = "@class.outer", desc = "Previous class end" },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
