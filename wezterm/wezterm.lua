acaf6f08ee119f6285a7992af57e7ad159d3e69c
Ryan Davis <ryan@rypac.org>
2023-05-15 07:33:37 +1000

    Add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "Gruvbox Material (Gogh)"
+config.front_end = "WebGpu"
+config.hide_tab_bar_if_only_one_tab = true
+config.show_update_window = true
+config.window_close_confirmation = "NeverPrompt"
+config.quit_when_all_windows_are_closed = false
+
+return config
