ebeeb2a3eaacaf1e7c39b7798fd6e965f36f7cfc
Ryan Davis <ryan@rypac.org>
2023-05-19 06:54:40 +1000

    Update wezterm configuration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Gruvbox Material (Gogh)"
+config.adjust_window_size_when_changing_font_size = false
+config.color_scheme = "Gruvbox dark, pale (base16)"
 config.front_end = "WebGpu"
 config.hide_tab_bar_if_only_one_tab = true
 config.show_update_window = true
