7043c621b8118c6671a30b7a6307dcd1a9a0d2cd
Ryan Davis <ryan@rypac.org>
2023-05-19 12:19:23 +1000

    Improve Haskell autocmd

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -20,9 +20,9 @@ vim.api.nvim_create_autocmd("FileType", {
   desc = "Automatically fold Haskell pragmas, exports and imports",
   pattern = "haskell",
   callback = function()
-    vim.cmd("silent! 1/^{-# LANGUAGE /normal zfip")
-    vim.cmd("silent! 1/^module /normal zfip")
-    vim.cmd("silent! 1/^import /normal zfip")
+    vim.cmd("silent! 1/^{-# LANGUAGE /normal! zfip``")
+    vim.cmd("silent! 1/^module /normal! zfip``")
+    vim.cmd("silent! 1/^import /normal! zfip``")
     vim.cmd("let @/ = ''")
   end,
 })
