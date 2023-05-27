c17c7e52807e0de65e171b6556123ec4d4ef0e1c
Ryan Davis <ryan@rypac.org>
2023-05-27 18:39:42 +1000

    Add dressing.nvim plugin

---
+++ nvim/lua/plugins/dressing.lua
@@ -0,0 +1,18 @@
+return {
+  "stevearc/dressing.nvim",
+  lazy = false,
+  opts = {
+    input = {
+      win_options = {
+        winblend = 0,
+      },
+    },
+    select = {
+      builtin = {
+        win_options = {
+          winblend = 0,
+        },
+      },
+    },
+  },
+}
