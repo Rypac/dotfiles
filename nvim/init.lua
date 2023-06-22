3a4c8f8dea14eb8543fa04076e7cea35ab750cfb
Ryan Davis <ryan@rypac.org>
2023-06-22 19:58:13 +1000

    Migrate neovim configuration to a single file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,84 @@
-require("core")
+-- UI
+vim.opt.cursorline = false
+vim.opt.termguicolors = vim.env.TERM_PROGRAM ~= "Apple_Terminal"
+
+-- Editing
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+-- Folding
+vim.opt.foldenable = false
+vim.opt.foldlevel = 99
+vim.opt.foldmethod = "manual"
+vim.opt.foldminlines = 0
+vim.opt.foldtext = "getline(v:foldstart)"
+
+-- General
+vim.opt.swapfile = false
+vim.opt.wildignore = { ".git/", "node_modules/" }
+
+-- Keybindings
+vim.g.mapleader = " "
+
+-- Search and replace
+vim.keymap.set("v", "<C-r>", [["hy:%s/\V<C-r>h//g<left><left>]], { desc = "Replace selected text" })
+
+-- Buffers
+vim.keymap.set("n", "<leader>bf", "<cmd>bfirst<cr>", { desc = "First buffer" })
+vim.keymap.set("n", "<leader>bl", "<cmd>blast<cr>", { desc = "Last buffer" })
+vim.keymap.set("n", "<leader>bn", "<cmd>bNext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", "<cmd>bPrevious<cr>", { desc = "Previous buffer" })
+
+-- Tabs
+vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Close tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tabfirst<cr>", { desc = "First tab" })
+vim.keymap.set("n", "<leader>tl", "<cmd>tablast<cr>", { desc = "Last tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnext<cr>", { desc = "Next tab" })
+vim.keymap.set("n", "<leader>tN", "<cmd>tabnew<cr>", { desc = "New tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
+
+-- Auto commands
+vim.api.nvim_create_autocmd("TermOpen", {
+  desc = "Configure UI for builtin terminal",
+  group = vim.api.nvim_create_augroup("ConfigureTerminal", { clear = true }),
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.signcolumn = "no"
+  end,
+})
+
+-- Plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins", {
+  defaults = {
+    lazy = true,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchparen",
+        "netrwPlugin",
+        "rplugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
