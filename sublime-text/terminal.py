a092ae41345ef8d82ab6d0c39deb90b181c7bc8c
Ryan Davis <ryan@rypac.org>
2023-10-11 13:25:00 +1100

    Add "Open in Terminal" Sublime plugin

---
+++ sublime-text/terminal.py
@@ -0,0 +1,43 @@
+import sublime
+import sublime_plugin
+import os
+import subprocess
+
+
+class OpenTerminalCommand(sublime_plugin.WindowCommand):
+    def run(self, paths=[], in_folder=False):
+        path = None
+
+        if paths:
+            path = paths[0]
+        elif (
+            not in_folder
+            and (view := self.window.active_view())
+            and (file_name := view.file_name())
+        ):
+            path = file_name
+        elif folders := self.window.folders():
+            path = folders[0]
+        else:
+            path = os.path.expanduser("~")
+
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+
+        try:
+            subprocess.run(
+                [
+                    "osascript",
+                    "-e",
+                    f'do shell script "open -a \'Terminal\' \\"{path}\\""',
+                ],
+                check=True,
+            )
+        except Exception as e:
+            sublime.error_message("Terminal: " + str(e))
+
+    def is_visible(self, paths=[], in_folder=False):
+        if in_folder:
+            return bool(self.window.folders())
+
+        return bool(paths or ((view := self.window.active_view()) and view.file_name()))
