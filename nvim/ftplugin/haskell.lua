e07d1ebf3385976e2335a65c5c5e9199380e6564
Ryan Davis <ryan@rypac.org>
2024-07-25 07:30:27 +1000

    Prefer functions to automatic folding of imports

--- nvim/ftplugin/haskell.lua
+++ nvim/ftplugin/haskell.lua
@@ -1,9 +1,17 @@
--- Automatically fold imports, exports and language pragmas
-vim.cmd([[
-  silent! normal! m`G
-  silent! /\_^{-#\s\+LANGUAGE\s\+/normal! mlzf}``m`
-  silent! /\_^module\s\+\_.\{-}where\s*\_$/normal! mevgnzf``m`
-  silent! /\_^import\s\+/normal! mizf}``m`
-  silent! /\_^main\s\+=/normal! mm``m`
-  let @/ = ""
-]])
+vim.api.nvim_buf_create_user_command(
+  0,
+  "FoldImports",
+  function()
+    vim.cmd([[
+      silent! normal! m`G
+      silent! /\_^{-#\s\+LANGUAGE\s\+/normal! mlzf}``m`
+      silent! /\_^module\s\+\_.\{-}where\s*\_$/normal! mevgnzf``m`
+      silent! /\_^import\s\+/normal! mizf}``m`
+      silent! /\_^main\s\+=/normal! mm``m`
+      let @/ = ""
+    ]])
+  end,
+  {
+    desc = "Fold imports, exports and language pragmas"
+  }
+)
--- nvim/ftplugin/kotlin.lua
+++ nvim/ftplugin/kotlin.lua
@@ -1,6 +1,14 @@
--- Automatically fold imports
-vim.cmd([[
-  silent! normal! m`G
-  silent! /\_^import\s\+/normal! mizf}``m`
-  let @/ = ""
-]])
+vim.api.nvim_buf_create_user_command(
+  0,
+  "FoldImports",
+  function()
+    vim.cmd([[
+      silent! normal! m`G
+      silent! /\_^import\s\+/normal! mizf}``m`
+      let @/ = ""
+    ]])
+  end,
+  {
+    desc = "Fold imports"
+  }
+)
