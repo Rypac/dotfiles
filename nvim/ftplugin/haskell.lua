c9ca7646cf62b9e07cfcb492fe2ce7b50f158fa7
Ryan Davis <ryan@rypac.org>
2024-07-24 21:35:30 +1000

    Fix automatic folding of module declaration

--- nvim/ftplugin/haskell.lua
+++ nvim/ftplugin/haskell.lua
@@ -2,7 +2,7 @@
 vim.cmd([[
   silent! normal! m`G
   silent! /\_^{-#\s\+LANGUAGE\s\+/normal! mlzf}``m`
-  silent! /\_^module\s\+\_.\{-})\_.\{-}where\s*\_$/normal! mevgnzf``m`
+  silent! /\_^module\s\+\_.\{-}where\s*\_$/normal! mevgnzf``m`
   silent! /\_^import\s\+/normal! mizf}``m`
   silent! /\_^main\s\+=/normal! mm``m`
   let @/ = ""
