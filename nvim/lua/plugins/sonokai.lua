1f9b78baab5cae877f8744931f93aaf16f8454f7
Ryan Davis <ryan@rypac.org>
2024-08-09 09:46:33 +1000

    Add ability to generate Sonokai colorschemes

---
+++ nvim/lua/plugins/sonokai.lua
@@ -0,0 +1,11 @@
+vim.g.sonokai_disable_italic_comment = 1
+
+for _, style in ipairs({ 'default', 'atlantis', 'andromeda', 'shusia', 'maia', 'espresso' }) do
+  vim.g.sonokai_style = style
+
+  require('mini.colors')
+    .get_colorscheme('sonokai', { new_name = 'sonokai-' .. style })
+    :add_cterm_attributes()
+    :add_terminal_colors()
+    :write()
+end
