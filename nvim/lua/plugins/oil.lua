95796ab6f1394e1a6755a48ec151982aab5a329c
Ryan Davis <ryan@rypac.org>
2024-08-08 17:09:38 +1000

    Show hidden files with oil by default

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,3 +1,7 @@
-require('oil').setup()
+require('oil').setup({
+  view_options = {
+    show_hidden = true
+  }
+})
 
 vim.keymap.set('n', '<Leader>-', '<cmd>Oil<cr>', { desc = 'Open parent directory' })
