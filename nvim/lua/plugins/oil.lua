2f737330217c84b0a55ff546ccfb1deab35ee525
Ryan Davis <ryan@rypac.org>
2024-08-12 15:47:05 +1000

    Update oil configuration

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,10 @@
 require('oil').setup({
+  skip_confirm_for_simple_edits = true,
   view_options = {
-    show_hidden = true
+    show_hidden = true,
+    is_always_hidden = function(name, bufnr)
+      return name == '..'
+    end
   }
 })
 
