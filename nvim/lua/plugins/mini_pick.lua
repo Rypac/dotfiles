3d70858cb9f36f26f57ce60aa2fc5295d9558e01
Ryan Davis <ryan@rypac.org>
2024-08-13 16:55:02 +1000

    Show mini.pick centered in the window

--- nvim/lua/plugins/mini_pick.lua
+++ nvim/lua/plugins/mini_pick.lua
@@ -3,8 +3,18 @@ pick.setup({
   source = {
     show = pick.default_show,
   },
-  options = {
-    content_from_bottom = true,
+  window = {
+    config = function()
+      height = math.floor(0.618 * vim.o.lines)
+      width = math.floor(0.618 * vim.o.columns)
+      return {
+        anchor = "NW",
+        height = height,
+        width = width,
+        row = math.floor(0.5 * (vim.o.lines - height)),
+        col = math.floor(0.5 * (vim.o.columns - width)),
+      }
+    end,
   },
 })
 
