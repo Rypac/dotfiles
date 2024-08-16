b5235f96fc7c5efde6164eb50276b7a404d41b33
Ryan Davis <ryan@rypac.org>
2024-08-16 22:42:44 +1000

    Configure mini.statusline sections

--- nvim/lua/user/plugins/mini-statusline.lua
+++ nvim/lua/user/plugins/mini-statusline.lua
@@ -1,5 +1,10 @@
-require("mini.statusline").setup({
+local statusline = require("mini.statusline")
+statusline.setup({
   use_icons = false,
 })
+
+statusline.section_location = function()
+  return "%2l|%L"
+end
 
 vim.opt.showmode = false
