c44ce2190083801e9c3ae294dcf9fcc1dcb5aad7
Ryan Davis <ryan@rypac.org>
2024-08-18 21:23:31 +1000

    Add completion for lazygit command

--- nvim/plugin/lazygit.lua
+++ nvim/plugin/lazygit.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_user_command("Lazygit", function(opts)
-  if opts.args then
+  if opts.args ~= "" then
     vim.cmd("terminal lazygit " .. opts.args)
   else
     vim.cmd("terminal lazygit")
@@ -7,12 +7,17 @@ vim.api.nvim_create_user_command("Lazygit", function(opts)
 end, {
   desc = "Open lazygit in a terminal",
   nargs = "*",
+  complete = function(arg, _, _)
+    return vim.tbl_filter(function(name)
+      return vim.startswith(name, arg)
+    end, { "status", "branch", "log", "stash" })
+  end,
 })
 
 vim.api.nvim_create_autocmd("TermClose", {
   desc = "Clear terminal on exiting Lazygit",
   pattern = "term://*lazygit*",
-  group = vim.api.nvim_create_augroup("QuitLazygit", { clear = true }),
+  group = vim.api.nvim_create_augroup("UserQuitLazygit", { clear = true }),
   callback = function(args)
     vim.cmd("bdelete! " .. args.buf)
   end,
