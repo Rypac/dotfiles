c415bc51def9632ec07c1352727f02ac65444848
Ryan Davis <ryan@rypac.org>
2024-08-21 21:21:39 +1000

    Use default material gruvbox colorscheme variant

--- nvim/lua/user/plugins/gruvbox-material.lua
+++ nvim/lua/user/plugins/gruvbox-material.lua
@@ -1,6 +1,5 @@
-vim.g.gruvbox_material_background = "hard"
-vim.g.gruvbox_material_foreground = "mix"
-vim.g.gruvbox_material_disable_italic_comment = 1
+vim.g.gruvbox_material_background = "medium"
+vim.g.gruvbox_material_foreground = "material"
 vim.g.gruvbox_material_better_performance = 1
 
 if vim.env.TERM_PROGRAM ~= "Apple_Terminal" then
