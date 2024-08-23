b82343d4b0987d30ffad44dc84227f1bd6b635ae
Ryan Davis <ryan@rypac.org>
2024-08-23 11:30:46 +1000

    Simplify grapple keymaps

--- nvim/lua/user/plugins/grapple.lua
+++ nvim/lua/user/plugins/grapple.lua
@@ -1,38 +1,10 @@
-local grapple = require("grapple")
-grapple.setup({
+require("grapple").setup({
   icons = false,
   style = "basename",
 })
 
-vim.keymap.set("n", ",", function()
-  grapple.toggle()
-end, {
-  desc = "Toggle a Grapple tag",
-})
-
-vim.keymap.set("n", "<Leader>,", function()
-  grapple.toggle_tags()
-end, {
-  desc = "Toggle Grapple tags window",
-})
-
-for index = 1, 9 do
-  vim.keymap.set("n", "<Leader>" .. index, function()
-    grapple.select({ index = index })
-  end, {
-    desc = "Select Grapple tag " .. index,
-  })
-end
-
-vim.keymap.set("n", "+", function()
-  vim.ui.input({ prompt = "Add a Grapple tag: " }, function(name)
-    if name ~= nil then
-      grapple.tag({ name = name })
-    end
-  end)
-end, {
-  desc = "Add a named Grapple tag",
-})
+vim.keymap.set("n", "+", "<Cmd>Grapple toggle<CR>", { desc = "Toggle a Grapple tag" })
+vim.keymap.set("n", "_", "<Cmd>Grapple toggle_tags<CR>", { desc = "Toggle Grapple tags window" })
 
 vim.api.nvim_create_autocmd("User", {
   desc = "Redraw statusline on Grapple update",
