9ef8166c368523d1dc5c7f23b717d791a2cb08c0
Ryan Davis <ryan@rypac.org>
2024-08-23 13:00:09 +1000

    Add custom session scope

--- nvim/lua/user/plugins/grapple.lua
+++ nvim/lua/user/plugins/grapple.lua
@@ -1,6 +1,25 @@
 require("grapple").setup({
   icons = false,
   style = "basename",
+  scope = "session",
+  scopes = {
+    {
+      name = "session",
+      desc = "Persisted session",
+      fallback = "git",
+      cache = {
+        event = { "DirChanged", "SessionLoadPost", "SessionWritePost" },
+      },
+      resolver = function()
+        local session = vim.v.this_session
+        if session == "" then
+          return
+        end
+
+        return session, vim.uv.cwd()
+      end,
+    },
+  },
 })
 
 vim.keymap.set("n", "+", "<Cmd>Grapple toggle<CR>", { desc = "Toggle a Grapple tag" })
