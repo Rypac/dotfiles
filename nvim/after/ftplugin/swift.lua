6b1e5ac7693e4be928a623199e98c90b981d48da
Ryan Davis <ryan@rypac.org>
2024-08-25 22:00:57 +1000

    Update swift ftplugin

--- nvim/after/ftplugin/swift.lua
+++ nvim/after/ftplugin/swift.lua
@@ -1,4 +1,9 @@
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+
 vim.opt_local.foldmethod = "expr"
 vim.opt_local.formatprg = "swift-format --assume-filename %"
+
+vim.opt_local.commentstring = "// %s"
 
 require("user.lsp").start("sourcekit")
