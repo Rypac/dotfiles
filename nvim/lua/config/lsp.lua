416744b747aee009797bbd9f3b97a1f768a214f6
Ryan Davis <ryan@rypac.org>
2025-04-05 08:28:27 +1100

    Fix lua formatting

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,11 +1,8 @@
-vim.lsp.enable(
-  {
-    "hls",
-    "lua_ls",
-    "sourcekit",
-  },
-  vim.g.lsp_autostart ~= false
-)
+vim.lsp.enable({
+  "hls",
+  "lua_ls",
+  "sourcekit",
+}, vim.g.lsp_autostart ~= false)
 
 vim.diagnostic.config({
   virtual_text = true,
