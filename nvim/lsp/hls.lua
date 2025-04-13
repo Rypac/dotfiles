1949228b8916a70c9ac41b887c5ad72d588dc21e
Ryan Davis <ryan@rypac.org>
2025-04-13 13:50:23 +1000

    Update LSP configuration

--- nvim/lsp/hls.lua
+++ nvim/lsp/hls.lua
@@ -1,14 +1,18 @@
 return {
   cmd = { "haskell-language-server-wrapper", "--lsp" },
   filetypes = { "haskell", "cabal" },
-  root_dir = function(_, callback)
-    local root_dir = vim.fs.root(0, function(name)
-      return name:match("%.cabal$") ~= nil
-    end)
+  root_dir = function(bufnr, callback)
+    local markers = { "hie.yaml", "stack.yaml", "cabal.project", "package.yaml" }
 
-    if root_dir ~= nil then
-      callback(root_dir)
-    end
+    callback(vim.fs.root(bufnr, function(name)
+      for _, marker in ipairs(markers) do
+        if marker == name then
+          return true
+        end
+      end
+
+      return name:match("%.cabal$") ~= nil
+    end))
   end,
   settings = {
     haskell = {
--- nvim/lsp/sourcekit.lua
+++ nvim/lsp/sourcekit.lua
@@ -2,4 +2,17 @@ return {
   cmd = { "xcrun", "sourcekit-lsp" },
   filetypes = { "swift" },
   root_markers = { "Package.swift", ".git" },
+  capabilities = {
+    workspace = {
+      didChangeWatchedFiles = {
+        dynamicRegistration = true,
+      },
+    },
+    textDocument = {
+      diagnostic = {
+        dynamicRegistration = true,
+        relatedDocumentSupport = true,
+      },
+    },
+  },
 }
