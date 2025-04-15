51ecc9924e55f3e923367f0be4438f179e388da8
Ryan Davis <ryan@rypac.org>
2025-04-16 06:00:48 +1000

    Update simple colorscheme

--- nvim/colors/simple.lua
+++ nvim/colors/simple.lua
@@ -1,10 +1,8 @@
 local is_dark = vim.o.background == "dark"
 
 require("mini.hues").setup({
-  accent = "bg",
   background = is_dark and "#122722" or "#dce4e8",
   foreground = is_dark and "#c0c9c6" or "#292f32",
-  n_hues = 8,
   saturation = is_dark and "medium" or "high",
   plugins = {
     default = false,
