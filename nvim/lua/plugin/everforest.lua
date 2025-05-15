0049670ba4c4da0979338dd4bb974d6a3aa3e2a3
Ryan Davis <ryan@rypac.org>
2025-05-15 15:22:53 +1000

    Remove unused theme configuration

--- nvim/lua/plugin/everforest.lua
+++
@@ -1,3 +0,0 @@
-vim.g.everforest_background = "hard"
-vim.g.everforest_ui_contrast = "high"
-vim.g.everforest_better_performance = 1
--- nvim/lua/plugin/gruvbox-material.lua
+++
@@ -1,4 +0,0 @@
-vim.g.gruvbox_material_background = "hard"
-vim.g.gruvbox_material_foreground = "mix"
-vim.g.gruvbox_material_ui_contrast = "high"
-vim.g.gruvbox_material_better_performance = 1
