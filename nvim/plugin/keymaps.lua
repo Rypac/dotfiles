c71a6e97e18f541e08d2fe18b079e74a93f3d881
Ryan Davis <ryan@rypac.org>
2025-08-15 22:16:00 +1000

    Call vim.keymap.set directly

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -1,29 +1,27 @@
-local map = vim.keymap.set
-
 -- Move by visible lines
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
 -- Clear search highlights
-map({ "i", "n" }, "<Esc>", "<Cmd>nohlsearch<CR><Esc>")
+vim.keymap.set({ "i", "n" }, "<Esc>", "<Cmd>nohlsearch<CR><Esc>")
 
 -- System clipboard
-map("x", "<Leader>y", '"+y', { desc = "Copy to clipboard" })
-map("n", "<Leader>y", '"+yy', { desc = "Copy to clipboard" })
-map({ "n", "x" }, "<Leader>p", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("x", "<Leader>y", '"+y', { desc = "Copy to clipboard" })
+vim.keymap.set("n", "<Leader>y", '"+yy', { desc = "Copy to clipboard" })
+vim.keymap.set({ "n", "x" }, "<Leader>p", '"+p', { desc = "Paste from clipboard" })
 
 -- Search in visual selection
-map("n", "<C-/>", "/\\%V", { desc = "Search in selection" })
-map("x", "<C-/>", "<Esc>/\\%V", { desc = "Search in selection" })
+vim.keymap.set("n", "<C-/>", "/\\%V", { desc = "Search in selection" })
+vim.keymap.set("x", "<C-/>", "<Esc>/\\%V", { desc = "Search in selection" })
 
 -- Tab navigation
-map("n", "]t", "<Cmd>tabnext<CR>", { desc = "Next tab" })
-map("n", "[t", "<Cmd>tabprevious<CR>", { desc = "Previous tab" })
-map("n", "]T", "<Cmd>tablast<CR>", { desc = "Last tab" })
-map("n", "[T", "<Cmd>tabfirst<CR>", { desc = "First tab" })
+vim.keymap.set("n", "]t", "<Cmd>tabnext<CR>", { desc = "Next tab" })
+vim.keymap.set("n", "[t", "<Cmd>tabprevious<CR>", { desc = "Previous tab" })
+vim.keymap.set("n", "]T", "<Cmd>tablast<CR>", { desc = "Last tab" })
+vim.keymap.set("n", "[T", "<Cmd>tabfirst<CR>", { desc = "First tab" })
 for tab = 1, 9 do
-  map("n", "<C-" .. tab .. ">", tab .. "gt", { desc = "Go to tab " .. tab })
-  map("i", "<C-" .. tab .. ">", "<C-o>" .. tab .. "gt", { desc = "Go to tab " .. tab })
+  vim.keymap.set("n", "<C-" .. tab .. ">", tab .. "gt", { desc = "Go to tab " .. tab })
+  vim.keymap.set("i", "<C-" .. tab .. ">", "<C-o>" .. tab .. "gt", { desc = "Go to tab " .. tab })
 end
 
 -- Option toggling
@@ -38,29 +36,29 @@ for key, option in pairs({
   w = "wrap",
   z = "spell",
 }) do
-  map("n", "<Leader>o" .. key, "<Cmd>setlocal " .. option .. "! " .. option .. "?<CR>", {
+  vim.keymap.set("n", "<Leader>o" .. key, "<Cmd>setlocal " .. option .. "! " .. option .. "?<CR>", {
     desc = "Toggle '" .. option .. "'",
   })
 end
-map(
+vim.keymap.set(
   "n",
   "<Leader>ob",
   "<Cmd>lua vim.o.background = vim.o.background == 'dark' and 'light' or 'dark'<CR><Cmd>set background?<CR>",
   { desc = "Toggle 'background'" }
 )
-map(
+vim.keymap.set(
   "n",
   "<Leader>of",
   "<Cmd>lua vim.b.lsp_autoformat = vim.b.lsp_autoformat == false<CR>",
   { desc = "Toggle 'autoformat'" }
 )
-map(
+vim.keymap.set(
   "n",
   "<Leader>os",
   "<Cmd>lua vim.o.signcolumn = vim.o.signcolumn == 'yes' and 'auto' or 'yes'<CR>",
   { desc = "Toggle 'signcolumn'" }
 )
-map(
+vim.keymap.set(
   "n",
   "<Leader>ot",
   "<Cmd>lua vim.o.showtabline = vim.o.showtabline == 1 and 2 or 1<CR>",
